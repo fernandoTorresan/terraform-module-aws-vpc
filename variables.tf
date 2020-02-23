7ce57940a0ee1339e10d131de83e5b32512291b6
Fernando Torresan <fernando.torresan@somospi.com.br>
2020-02-23 16:05:40 -0300

    Declaring variables used within the module

---
+++ variables.tf
@@ -0,0 +1,97 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# VARIÁVEIS OBRIGATÓRIAS
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "name" {
+  type        = string
+  description = "Nome da VPC"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = ""
+}
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# VARIÁVEIS OPICIONAIS
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "single_nat_gateway" {
+  type        = bool
+  default     = false
+  description = "Se for configurada como true, existirá apenas um NAT Gateway."
+}
+
+variable "one_nat_gateway_per_az" {
+  type        = bool
+  default     = true
+  description = "Define se será criado um NAT Gateway por zona de disponibilidade da região da VPC."
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "Lista de subnets (CIDR) privadas que irão pertecer a VPC."
+  default     = []
+}
+
+variable "map_public_ip_on_launch" {
+  type        = bool
+  default     = true
+  description = "Se deseja que não seja vinculado automaticamente um IP público ao subir uma instância, deve-se ser informado como false."
+}
+
+variable "private_subnet_suffix" {
+  type        = string
+  default     = "private"
+  description = "Sufixo que será concatenado com o nome das subnets privadas."
+}
+
+variable "public_subnet_suffix" {
+  type        = string
+  default     = "public"
+  description = "Sufixo que será concatenado com o nome das subnets públicas."
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  default     = []
+  description = "Lista de subnets (CIDR) públicas que irão pertencer a VPC."
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "Uma lista das zonas de disponibilidade da região."
+  default     = []
+}
+
+variable "enable_classiclink" {
+  default = ""
+}
+variable "enable_classiclink_dns_support" {
+  default = ""
+}
+variable "enable_dns_hostnames" {
+  default = ""
+}
+
+variable "enable_dns_support" {
+  default = ""
+}
+
+variable "instance_tenancy" {
+  default = "default"
+}
+
+variable "enable_ipv6" {
+  type        = bool
+  default     = false
+  description = "Habilita ipv6"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags adicionais"
+}
