db18e6b11d52e89e2a5ea9fa5d6ba3e41e1bc8b0
Fernando Torresan <fernandotdo61@gmail.com>
2020-02-24 13:19:12 -0300

    Adding new module's outputs and their descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "vpc_id" {
-  value = aws_vpc.this.id
+  value       = aws_vpc.this.id
+  description = "ID da VPC."
+}
+
+output "vpc_cidr" {
+  value       = aws_vpc.this.cidr_block
+  description = "VPC CIDR."
 }
