695cf1e9f095e22a2b6c43ed4f13510ce7825b8c
Fernando Torresan <fernandotdo61@gmail.com>
2020-02-28 22:42:00 -0300

    Update simple usage example vpc module

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 module "simple_vpc" {
@@ -10,11 +10,11 @@ module "simple_vpc" {
 
   single_nat_gateway = true
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["20.0.1.0/24", "20.0.2.0/24", "20.0.3.0/24"]
-  public_subnets  = ["20.0.101.0/24", "20.102.0/24", "20.0.103.0/24"]
+  public_subnets  = ["20.0.101.0/24", "20.0.102.0/24", "20.0.103.0/24"]
 
   tags = {
-    "ManagedBy" = Terraform
+    ManagedBy = "Terraform"
   }
 }
