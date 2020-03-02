454f3cd46113181aae8b3079b7b7e1c741390f4b
Fernando Torresan <fernandotdo61@gmail.com>
2020-03-01 22:54:40 -0300

    Adding new outputs in example of simple vpc

--- examples/simple/outputs.tf
+++ examples/simple/outputs.tf
@@ -5,3 +5,31 @@ output "vpc_id" {
 output "vpc_cidr_bloc" {
   value = module.simple_vpc.vpc_cidr_block
 }
+
+output "vpc_arn" {
+  value = module.simple_vpc.vpc_arn
+}
+
+output "private_subnets" {
+  value = module.simple_vpc.private_subnets
+}
+
+output "private_subnets_cidr_blocks" {
+  value = module.simple_vpc.private_subnets_cidr_blocks
+}
+
+output "public_subnets" {
+  value = module.simple_vpc.public_subnets
+}
+
+output "public_subnets_cidr_blocks" {
+  value = module.simple_vpc.public_subnets_cidr_blocks
+}
+
+output "nat_gateway" {
+  value = module.simple_vpc.natgw_ids
+}
+
+output "internet_gateway" {
+  value = module.simple_vpc.igw_id
+}
